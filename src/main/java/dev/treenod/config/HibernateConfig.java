a40ac28361533ad19a1621848e4cddaea9628b38
treenod_anderson <jinwoo03@treenod.com>
2023-02-27 11:47:36 +0900

    hibernate config 추가 및 테스트

---
+++ src/main/java/dev/treenod/config/HibernateConfig.java
@@ -0,0 +1,84 @@
+package dev.treenod.config;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfig {
+
+    public HibernateConfig() {
+    }
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.jpa.database-platform}")
+    private String databasePlatform;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddlAuto;
+
+    @Value("${spring.jpa.show_sql}")
+    private Boolean showSql;
+
+    @Value("${spring.jpa.hibernate.format_sql}")
+    private Boolean formatSql;
+
+    @Bean
+    public DataSource getDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception{
+        Properties properties = new Properties();
+
+        properties.put("hibernate.dialect", databasePlatform);
+        properties.put("hibernate.show_sql", showSql);
+        properties.put("current_session_context_class", "thread");
+        properties.put("hibernate.format_sql", formatSql);
+        properties.put("hibernate.hbm2ddl.auto", ddlAuto);
+
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+
+        sessionFactoryBean.setPackagesToScan("dev.treenod.*");
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setHibernateProperties(properties);
+        sessionFactoryBean.afterPropertiesSet();
+
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory){
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+        transactionManager.setDataSource(getDataSource());
+        return transactionManager;
+    }
+}
---
+++ src/main/java/dev/treenod/domain/User.java
@@ -0,0 +1,17 @@
+package dev.treenod.domain;
+
+import javax.persistence.*;
+
+@Entity(name = "user")
+public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column
+    private String email;
+}
