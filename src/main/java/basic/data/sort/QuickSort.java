3fdeb556aad2634ee03c1c658689f797c78821db
leejinwoo <leejinwoo1126@gmail.com>
2023-08-15 22:49:42 +0900

    fix : QuickSort 버그 수정 완료

--- src/main/java/basic/data/sort/QuickSort.java
+++ src/main/java/basic/data/sort/QuickSort.java
@@ -1,121 +1,105 @@
 package basic.data.sort;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Random;
 
-
+// https://www.youtube.com/watch?v=h8eyY7dIiN4
+// 1 8 3 9 4 5 7 -- pivot = 7
+// L = 8; R = 5
+// 1 5 3 9 4 8 7
+// L = 9, R = 4
+// 1 5 3 4 9 8 7
+// L = 9, R = 9 에서 정지
+// 1 5 3 4 9 8 7 -- L == R인 위치와 pivot 스왑
+// 파티션을 나누기 전 피벗 기준으로 좌측은 작고, 우측은 큰 값이 위치하게 됨
+// 결과적으로 파티션을 나누기 전에 피벗 기준으로 나눠지므로, 가장 작은 파티션이 되면 이미 완료가 됨 (피벗 기준 그룹 나눈 후 파티션)
+// <-> 병합 정렬은 우선 파티션을 가장 작게 나누고 병합하면서 정렬하는 방식 (분할 후 정렬)
 public class QuickSort {
 
-    // 마지막 인덱스를 pivot 으로 지정하는 경우
-    // https://st-lab.tistory.com/250
-    private static void quickSortByRightPivot(List<Integer> arr, int leftIdx, int rightIdx) {
-        if(leftIdx >= rightIdx) return;
-
-        int pivot = arr.get(rightIdx);
-        int L = leftIdx;
-        int R = rightIdx - 1;
-
-        while(L < R) {
-            while(arr.get(L) <= pivot && L < R) L += 1;
-
-            while(pivot <= arr.get(R) && L < R) R -= 1;
-
-            Collections.swap(arr, L, R);
-        }
-
-        Collections.swap(arr, L, rightIdx); // L == R 일때 피벗과 스왑
-
-        quickSortByRightPivot(arr, leftIdx, L - 1);
-        quickSortByRightPivot(arr, L + 1, rightIdx);
+    private static void quickSort(int[] arr) {
+        //quickSortByLeftPivot(arr, 0, arr.length - 1);
+        quickSortByMidPivot(arr, 0, arr.length - 1);
+        //quickSortByRightPivot(arr, 0, arr.length - 1);
     }
 
-    // 2 77 60 84 79 86 46 22 22 와 같이
-    // 왼쪽 피벗이 제일 제일 작은 경우 이슈가 발생함
-    //https://hongl.tistory.com/5
-    private static void quickSortByLeftPivot(List<Integer> arr, int leftIdx, int rightIdx) {
+    private static void swap(int[] arr, int left, int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+
+    // left의 경우 R이 먼저 움직이고, R을 기준으로 파티션 나눠짐
+    // 1 8 3 9 4 5 7 에서 최소값이 이미 왼쪽에 있는 경우 생각해보기
+    private static void quickSortByLeftPivot(int[] arr, int leftIdx, int rightIdx) {
         if(leftIdx >= rightIdx) return;
 
-        int pivot = arr.get(leftIdx);
-        int L = leftIdx + 1;
+        int pivot = arr[leftIdx];
+        int L = leftIdx;
         int R = rightIdx;
 
-        while(L <= R) {
-            // right pivot과 비교시 범위가 다르네 ..
-            while(arr.get(L) <= pivot && L < rightIdx) L += 1;
-            while(pivot <= arr.get(R) && leftIdx < R) R -= 1;
+        while(L < R) {
+            while(pivot < arr[R] && L < R) R -= 1;
+            while(arr[L] < pivot && L < R) L += 1;
 
             if(L >= R) break;
 
-            Collections.swap(arr, L, R);
+            swap(arr, L, R);
         }
 
-        Collections.swap(arr, leftIdx, R); // 여기서 꼬이네 2 58 .. 2보다 큰 경우 R = 1, L = 1되고 나니 스왑하면망함
-
-        if(leftIdx < L) quickSortByLeftPivot(arr, leftIdx, R - 1);
-        if(L < rightIdx) quickSortByLeftPivot(arr, R + 1, rightIdx);
+        quickSortByLeftPivot(arr, leftIdx, R - 1);
+        quickSortByLeftPivot(arr, R + 1, rightIdx);
     }
 
-
-    private static void quickSort(List<Integer> arr, int low, int high) {
-        if(low >= high) return;
-
-        int pivot = partition(arr, low, high);
-        quickSort(arr, low, pivot - 1);
-        quickSort(arr, pivot + 1, high);
-    }
-
-    private static int partition(List<Integer> arr, int low, int high) {
-        int pivot = arr.get(low);
-        int i = low;
-        for(int j = i + 1; j <= high; j++) {
-            if(pivot > arr.get(j)) {
-                i += 1;
-                Collections.swap(arr, i, j);
-            }
-        }
-
-        Collections.swap(arr, low, i);
-
-        return i - 1;
-    }
-
-    // 중간 인덱스를 pivot 으로 지정하는 경우
-    // https://www.youtube.com/watch?v=7BDzle2n47c&t=395s
-    private static void quickSortByMiddlePivot(List<Integer> arr, int leftIdx, int rightIdx) {
+    // 오른쪽 피벗 기준인 경우 L 부터 움직이고, L을 기준으로 파티션 나눔
+    private static void quickSortByRightPivot(int[] arr, int leftIdx, int rightIdx) {
         if(leftIdx >= rightIdx) return;
 
-        int pl = leftIdx;
-        int pr = rightIdx;
+        int pivot = arr[rightIdx];
+        int L = leftIdx;
+        int R = rightIdx;
 
-        // 같은 경우 swap하더라도 변화없고 pl > pr 로 교차하게 됨
-        int mid = (pl + pr) / 2;
-        int pivot = arr.get(mid); // 한번 피벗 값은 해당 파티션에서 고정**
-        while(pl <= pr) { // 이 괄호가 문제였는듯
-            while(arr.get(pl) < pivot) pl += 1; // 크거나 같은 값을 찾음
-            while(pivot < arr.get(pr)) pr -= 1; // 작거나 같은 값을 찾음
+        while(L < R) {
+            while(arr[L] < pivot && L < R) L += 1;
+            while(pivot < arr[R] && L < R) R -= 1;
 
-            if(pl <= pr) {
-                Collections.swap(arr, pl, pr);
-                pl += 1;
-                pr -= 1;
+            while(L >= R) break;
+
+            swap(arr, L, R);
+        }
+
+        quickSortByRightPivot(arr, leftIdx, R - 1);
+        quickSortByRightPivot(arr, R + 1, rightIdx);
+    }
+
+    // 중간 피벗의 경우 L, R이 엇갈릴 때까지 수행
+    private static void quickSortByMidPivot(int[] arr, int leftIdx, int rightIdx) {
+        if(leftIdx >= rightIdx) return;
+
+        int mid = (leftIdx + rightIdx) / 2;
+        int pivot = arr[mid];
+        int L = leftIdx;
+        int R = rightIdx;
+
+        while(L <= R) {
+            while(pivot < arr[R]) R -= 1;
+            while(arr[L] < pivot) L += 1;
+
+            if(L <= R) {
+                swap(arr, L, R);
+                L += 1;
+                R -= 1;
             }
         }
 
-        // 파티션
-        if(leftIdx < pr) quickSortByMiddlePivot(arr, leftIdx, pr);
-        if(pl < rightIdx) quickSortByMiddlePivot(arr, pl, rightIdx);
+        quickSortByRightPivot(arr, leftIdx, R);
+        quickSortByRightPivot(arr, L, rightIdx);
     }
 
     public static void main(String[] args) {
-        List<Integer> data = new ArrayList<>();
+        int[] data = new Random().ints(1, 99).distinct().limit(7).toArray();
 
-        for(int i = 1; i <= 9; i++) {
-            data.add((int)(Math.random() * 100));
-        }
-
-        System.out.println(data);
-        quickSortByLeftPivot(data, 0, data.size() - 1);
-        System.out.println(data);
+        System.out.println(Arrays.toString(data));
+        quickSort(data);
+        System.out.println(Arrays.toString(data));
     }
 }
