9467880280adb26d095c9f47d3d853561191c5f5
이진우 <leejinwoo1126@gmail.com>
2024-02-08 17:39:30 +0900

    자료구조: 우선 순위 큐 Comparable 추가

--- src/main/java/basic/data/structure/heap/MyPriorityQueue.java
+++ src/main/java/basic/data/structure/heap/MyPriorityQueue.java
@@ -2,7 +2,6 @@ package basic.data.structure.heap;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.PriorityQueue;
 
 /**
  * PriorityQueue
@@ -21,21 +20,20 @@ public class MyPriorityQueue<E> {
         this(DEFAULT_INITIAL_CAPACITY, null);
     }
 
+    public MyPriorityQueue(int initialCapacity) {
+        this(initialCapacity, null);
+    }
+
+    public MyPriorityQueue(Comparator<? super E> comparator) {
+        this(DEFAULT_INITIAL_CAPACITY, comparator);
+    }
+
     public MyPriorityQueue(int initialCapacity, Comparator<? super E> comparator) {
         if(initialCapacity < 1)
             throw new IllegalArgumentException();
 
         this.queue = new Object[initialCapacity];
         this.comparator = comparator;
-    }
-
-    public void clear() {
-        final Object[] es = this.queue;
-        for(int i = 0, n = this.size; i < n; i++) {
-            es[i] = null;
-        }
-
-        this.size = 0;
     }
 
     public boolean add(E e) {
@@ -46,69 +44,108 @@ public class MyPriorityQueue<E> {
         if(e == null)
             throw new NullPointerException();
 
-        int i = this.size;
+        int i = size;
 
-        // i >= queue.length 경우 사이즈를 i + 1 증가 시킴(생략)
+        // i >= queue.length 인 경우 사이즈 증가
+
         siftUp(i, e);
         this.size = i + 1;
         return true;
     }
 
-    private void siftUp(int k, E x) { // k : 마지막 인덱스, x : 삽입할 데이터
-        siftUpComparable(k, x, queue);
+    private void siftUp(int k, E x) { // k : size , x : add Element
+        if(this.comparator != null)
+            siftUpUsingComparator(k, x, this.queue, this.comparator);
+        else
+            siftUpComparable(k, x, this.queue);
     }
 
-    //마지막 인덱스에 추가되고, 부모 노드와 비교하며 heapify
-    //Integer 라고 한다면 기본 최소힙으로 예상됨
-    private static <T> void siftUpComparable(int k, T x, Object[] es) {
-        Comparable<? super T> key = (Comparable<? super T>) x; // T타입 : Comparable 구현된 Wrapper class
+    // k : current size, x : add valud,
+    private static <T> void siftUpUsingComparator(int k, T x, Object[] es, Comparator<? super T> comparator) {
         while(k > 0) {
-            int parent = (k - 1) >>> 1; // k / 2
+            int parent = (k - 1) >>> 1; // 3이면 부모는 1이됨
+            Object e = es[parent];
+
+            if(comparator.compare(x, (T) e) >= 0)
+                break;
+
+            es[k] = e;
+            k = parent;
+        }
+        es[k] = x;
+    }
+
+    // 마지막 노드에 삽입 후 up
+    private static <T> void siftUpComparable(int k, T x, Object[] es) {
+        Comparable<? super T> key = (Comparable<? super T>)  x;
+        while(k > 0) {
+            int parent = (k - 1) >>> 1; // 2개 있을 때 부모는 0번 노드
             Object e = es[parent];
             if(key.compareTo((T) e) >= 0)
                 break;
             es[k] = e;
             k = parent;
         }
-
-        es[k] = key;
+        es[k] = key; // k = 0 일때,
     }
 
     public E poll() {
         final Object[] es;
         final E result;
 
-        if((result = (E)(es = this.queue)[0]) != null) { // 루트값
+        if((result = (E)((es = this.queue)[0])) != null) {
             final int n;
-            final E x = (E) es[(n = --this.size)]; // 마지막 값
+            final E x = (E) es[n = --size]; // 마지막 인덱스에 값을 꺼냄
             es[n] = null; // 마지막 인덱스 비움
             if(n > 0) {
-                // Comparator 있는 경우, 없는 경우 나눠짐(우선 생략)
-                siftDownComparable(0, x, es, n);
+                final Comparator<? super E > cmp;
+                if((cmp = comparator) == null)
+                    siftDownComparable(0, x, es, n); //
+                else
+                    siftDownUsingComparator(0, x, es, n, cmp);
             }
         }
 
         return result;
     }
 
-    // k : 시작점 (0), x : 마지막값, es : 큐, n : 배열 사이즈
-    private static <T> void siftDownComparable(int k, T x, Object[] es, int n) {
-        Comparable<? super T> key = (Comparable<? super T>) x;
-        int half = n >>> 1; // 가장 마지막 요소의 부모 인덱스
-        while(k < half) { // half 이후는 리프노드이므로, sift연산을 할 필요가 없음
-            int child = (k << 1) + 1; // 루트 0번부터 시작이면, 왼쪽 자식
+    private static <T> void siftDownUsingComparator(int k, T x, Object[] es, int n, Comparator<? super T> cmp) {
+        int half = n >>> 1; // half부터 리프 노드
+        while(k < half) {
+            int child = (k << 1) + 1; // left
             Object c = es[child];
             int right = child + 1;
 
-            // 자식이 두명있고, left > right 일때, 최소힙이라면 c는 right가 되는게 맞다
-            if(right < n &&
-                    ((Comparable<? super T>)c).compareTo((T) es[right]) > 0) {
+            if(right < n && ((Comparable<? super T>)c).compareTo((T) es[right]) > 0) {
                 c = es[child = right];
             }
 
-            if(key.compareTo((T) c) <= 0) {
+            if(cmp.compare(x, (T) c) <= 0)
                 break;
+
+            es[k] = c;
+            k = child;
+        }
+
+        es[k] = x;
+    }
+
+    // k : 루트노드(0 고정), x : 마지막 인덱스 값, n = 현재 사이즈 - 1
+    private static <T> void siftDownComparable(int k, T x, Object[] es, int n) {
+        Comparable<? super T> key = (Comparable<? super T>) x;
+        int half = n >>> 1; // size의 절반, 해당 노드 리프노드
+        while(k < half) {
+            int child = (k << 1) + 1; // 2배 + 1, left child
+            Object c = es[child];
+            int right = child + 1;
+
+            // 자식이 둘다 존재할 때
+            if(right < n && ((Comparable<? super T>) c).compareTo((T) es[right]) > 0) {
+                c = es[child = right];
             }
+
+            if(key.compareTo((T) c) <= 0)
+                break;
 
             es[k] = c;
             k = child;
@@ -121,16 +158,36 @@ public class MyPriorityQueue<E> {
     }
 
     public static void main(String[] args) {
-        MyPriorityQueue<Integer> que = new MyPriorityQueue<>();
-        que.add(3);
-        que.add(2);
-        que.add(1);
-        que.add(4);
-        que.add(5);
+        // 오름차순 : 1 2 4 5 3
+        // 내림차순 : 5 4 2 1 3
+        MyPriorityQueue<Integer> asc = new MyPriorityQueue<>(); // 오름차순
+        asc.add(5);
+        asc.add(4);
+        asc.add(3);
+        asc.add(2);
+        asc.add(1);
 
-        System.out.println(Arrays.toString(que.toArray())); // [1,3,2,4,5]
+        System.out.println(Arrays.toString(asc.toArray()));
+        System.out.println(asc.poll());
+        System.out.println(asc.poll());
+        System.out.println(asc.poll());
+        System.out.println(asc.poll());
+        System.out.println(asc.poll());
 
-        que.poll();
-        System.out.println(Arrays.toString(que.toArray())); // [2,3,5,4]
+
+        MyPriorityQueue<Integer> desc = new MyPriorityQueue<>((a, b) -> b - a); // 내림차순
+        desc.add(1);
+        desc.add(2);
+        desc.add(3);
+        desc.add(4);
+        desc.add(5);
+
+        System.out.println(Arrays.toString(desc.toArray()));
+
+        System.out.println(desc.poll());
+        System.out.println(desc.poll());
+        System.out.println(desc.poll());
+        System.out.println(desc.poll());
+        System.out.println(desc.poll());
     }
 }
