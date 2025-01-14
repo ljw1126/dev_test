b26f6caf0bf084a2ac145bd69ddd69dc197684c4
이진우 <leejinwoo1126@gmail.com>
2025-01-14 17:52:14 +0900

    백준 문제 풀이용 Practice 탬플릿 추가

---
+++ src/main/java/backjun/Practice.java
@@ -0,0 +1,73 @@
+package backjun;
+
+import java.io.*;
+import java.util.*;
+
+public class Practice {
+    private static StringBuilder sb = new StringBuilder();
+    private static InputProcessor inputProcessor = new InputProcessor();
+
+    public static void main(String[] args) {
+        input();
+        pro();
+        output();
+    }
+
+    private static void input() {
+
+    }
+
+    private static void pro() {
+
+    }
+
+    private static void output() {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            bw.write(sb.toString());
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static class InputProcessor {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        public InputProcessor() {
+            this.br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        public String nextLine() {
+            String result = "";
+
+            try {
+                result = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return result;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
