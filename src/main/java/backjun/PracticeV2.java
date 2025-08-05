d569a44115fe725c8998ded40086f1afc31baee7
leejinwoo <leejinwoo1126@gmail.com>
2025-08-05 11:13:30 +0900

    PracticeV2 초기화

---
+++ src/main/java/backjun/PracticeV2.java
@@ -0,0 +1,34 @@
+package backjun;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class PracticeV2 {
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+        output();
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+
+    private static void pro() {
+
+    }
+
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
